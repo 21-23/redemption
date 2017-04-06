2b56435a16a0b28cd091ac00ac6e6864cae864e8
DaQuirm <leo.daquirm@gmail.com>
2017-04-07 01:14:15 +0200

    :sparkles: Serialize time values as integers, not stupid lists

--- src/NominalDiffTimePersistField.hs
+++ src/NominalDiffTimePersistField.hs
@@ -6,17 +6,14 @@ module NominalDiffTimePersistField where
 import Database.Persist.Sql
 import Data.Time.Clock (NominalDiffTime)
 import Data.Text (pack)
-import Data.Ratio
+import Data.Convertible.Base
+import Data.Convertible.Instances.Time()
 
 instance PersistFieldSql NominalDiffTime where
   sqlType _ = SqlReal
 
 instance PersistField NominalDiffTime where
-  toPersistValue timeDiff =
-    let rational = toRational timeDiff
-        num = PersistInt64 $ fromInteger $ numerator rational
-        den = PersistInt64 $ fromInteger $ denominator rational
-    in PersistList [num, den]
+  toPersistValue timeDiff = PersistInt64 $ fromInteger $ convert timeDiff
 
-  fromPersistValue (PersistList [PersistInt64 num, PersistInt64 den]) = Right $ fromRational $ toInteger num % toInteger den
+  fromPersistValue (PersistInt64 value) = Right $ convert $ toInteger value
   fromPersistValue value = Left $ pack ("Failed to parse value" ++ show value)
